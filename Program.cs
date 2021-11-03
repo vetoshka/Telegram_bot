956fa4a3ae3b37ed3511066a64cfd7b829394c80
Anna Kolomiiets <Anna_Kolomiiets@epam.com>
2021-11-04 01:18:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Telegram_bot
+{
+    class Program
+    {
+        private static string Token { get; set; } = "2096779250:AAGLwigmPaI03HeU2Fd2Lbgm0NNFUPxkiEg";
+        private static TelegramBotClient client;
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(Token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+
+                await client.SendTextMessageAsync(msg.Chat.Id, msg.Text );
+            }
+        }
+
+    }
+}
